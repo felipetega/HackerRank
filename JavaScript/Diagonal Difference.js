472e9fe112f16b88cc25762dad8e1dad4def6556
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-05-31 12:55:33 -0300

    rank

--- JavaScript/Diagonal Difference.js
+++ JavaScript/Diagonal Difference.js
@@ -1,7 +1,9 @@
 function diagonalDifference(arr) {
   // Write your code here
-  //var diagonal1=arr[0][0]+arr[1][1]+arr[2][2]
-  //var diagonal2=arr[0][2]+arr[1][1]+arr[2][0]
+  //diagonal1=arr[0][0]+arr[1][1]+arr[2][2]
+  //             [i][i]    [i][i]    [i][i]
+  //diagonal2=arr[0][2]+arr[1][1]+arr[2][0]
+  //             [i][i2]   [i][i2]   [i][i2]
   var diagonal1=0
   var diagonal2=0
   var i2=arr.length-1
@@ -11,7 +13,7 @@ function diagonalDifference(arr) {
       diagonal2+=arr[i][i2]
       i2--
   }
-  //returns   
+  //return the difference   
   if(diagonal1>diagonal2){
       return diagonal1-diagonal2
   }else if(diagonal2>diagonal1){
